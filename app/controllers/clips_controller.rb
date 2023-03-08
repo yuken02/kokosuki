0e9d50310df7d4d0610f0381301c0753ced67a06
yuken02 <git@github.com:yuken02/practice.git>
2023-03-08 15:22:30 +0000

    [Add]clipページ準備

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,4 +1,10 @@
 class ClipsController < ApplicationController
   def index
+    @clip_new = Clip.new
+    @clips = Clip.all
+  end
+  
+  def create
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  resources :clips, only: [:index]
+  resources :clips, only: [:index,:show,:new,:create,:edit,:update,:destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
