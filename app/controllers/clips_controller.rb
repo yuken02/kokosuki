caa9ac7e65c193077d6951dc2b500d9772347ecc
yuken02 <git@github.com:yuken02/practice.git>
2023-03-08 19:27:51 +0000

    [Add]動画情報・チャンネル情報取得テスト

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -2,9 +2,61 @@ class ClipsController < ApplicationController
   def index
     @clip_new = Clip.new
     @clips = Clip.all
+
+    require 'google/apis/youtube_v3'
+    # require 'google/api_client'
+    require 'date'
+
+    api_key = ENV['YOUTUBE_API_KEY']
+
+    @url = params[:url]
+    if @url
+      if @url.include?("youtube.com/watch?v=")
+        video_id = @url.split("v=").last
+      elsif @url.include?("youtu.be/")
+        video_id = @url.split("/").last
+      elsif @url.include?("youtube.com/live/")
+        video_id = @url.split("/").last.split("?").first
+      end
+
+      youtube = Google::Apis::YoutubeV3::YouTubeService.new
+      youtube.key = api_key
+
+      # 動画情報を取得する
+      video = youtube.list_videos("snippet", id: video_id).items.first
+      @video_id = video_id
+      # 投稿日、チャンネル名、チャンネルURLを取得する
+      @published_at = video.snippet.published_at
+      @channel_title = video.snippet.channel_title
+      @channel_url = "https://www.youtube.com/channel/#{video.snippet.channel_id}"
+    end
   end
-  
+
   def create
-    
+    # require 'google/apis/youtube_v3'
+    # # require 'google/api_client'
+    # require 'date'
+
+    # api_key = ENV['YOUTUBE_API_KEY']
+
+    # if url.include?("youtube.com/watch?v=")
+    #   video_id = url.split("v=").last
+    # elsif url.include?("youtu.be/")
+    #   video_id = url.split("/").last
+    # elsif url.include?("youtube.com/live/")
+    #   video_id = url.split("/").last.split("?").first
+    # end
+
+    # youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    # youtube.key = api_key
+
+    # # 動画情報を取得する
+    # video = youtube.list_videos("snippet", id: video_id).items.first
+
+    # # 投稿日、チャンネル名、チャンネルURLを取得する
+    # @published_at = video.snippet.published_at
+    # @channel_title = video.snippet.channel_title
+    # @channel_url = "https://www.youtube.com/channel/#{video.snippet.channel_id}"
+
   end
 end
