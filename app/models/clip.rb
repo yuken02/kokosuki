ee23bf6177ffb04a446dc5daebc5fbf38c1e6f07
yuken02 <git@github.com:yuken02/practice.git>
2023-03-09 11:05:30 +0000

    [Add]モデルの関連付け

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,2 +1,7 @@
 class Clip < ApplicationRecord
+  belongs_to :user
+  belongs_to :channel
+  has_many :favorites, dependent: :destroy
+  has_many :playlist_video, dependent: :destroy
+  has_many :tagging, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :playlist, dependent: :destroy
+  has_many :clip, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
 end
