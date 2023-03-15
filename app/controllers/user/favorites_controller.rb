97f1b4422978f3b7bd48adcd616c8d635d61162d
yuken02 <you.can.live0201@gmail.com>
2023-03-15 20:07:26 +0000

    [Add]userページ テンプレート切り替え機能、アコーディオン

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -4,8 +4,6 @@ class User::FavoritesController < ApplicationController
     favorite = current_user.favorites.new(clip_id: clip.id)
     favorite.save
     @clip = Clip.includes(:user, :channel, :favorites).find_by(id: params[:clip_id])
-    # @clip = Clip.includes(:user, :channel, :favorites).find(params[:clip_id])
-    # @clip_new = Clip.new
   end
 
   def destroy
@@ -14,7 +12,5 @@ class User::FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(clip_id: clip.id)
     favorite.destroy
     @clip = Clip.includes(:user, :channel, :favorites).find_by(id: params[:clip_id])
-    # @clip = Clip.includes(:user, :channel, :favorites).find(params[:clip_id])
-    # @clip_new = Clip.new
   end
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -3,7 +3,39 @@ class User::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @clips = Clip.where(user_id: @user.id).includes(:channel, :favorites)
+
+        # case params[:template]
+        #   when 'clips_list'
+        #     @template = 'clips_list'
+        #   when 'user'
+        #     @template = 'user'
+        #   when 'playlist'
+        #     @template = 'playlist'
+        #   else
+        #     @template = 'clips_list'
+        # end
+        case params[:template]
+          when 'clips_list'
+          @template = 'user/users/clips_list'
+          when 'user'
+          @template = 'user/users/user'
+          when 'playlist'
+          @template = 'user/users/playlist'
+          else
+          @template = 'user/users/clips_list'
+        end
+    respond_to do |format|
+      format.html
+      format.js { render 'switch.js.erb' }
+    end
   end
+
+  # def switch
+  #   @template_name = params[:template_name]
+  #   respond_to do |format|
+  #     format.js
+  #   end
+  # end
 
   def edit
 
