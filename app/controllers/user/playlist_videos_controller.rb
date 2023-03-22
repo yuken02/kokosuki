560f03c085a2e8dc9cd5f8a3ed1f114b6b7b546d
yuken02 <you.can.live0201@gmail.com>
2023-03-22 20:09:55 +0000

    [Update]clipId取得成功

--- app/controllers/user/playlist_videos_controller.rb
+++ app/controllers/user/playlist_videos_controller.rb
@@ -2,7 +2,7 @@ class User::PlaylistVideosController < ApplicationController
 
   def create
     # add_playlist = Playlist_video.find(params[:playlist_id][:clip_id])
-    add_playlist = Playlist_video.new(playlist_id: params[:playlist_id], clip_id: params[:clip_id])
+    add_playlist = PlaylistVideo.new(playlist_id: params[:playlist_id], clip_id: params[:clip_id])
     # add_to_playlist =
     if add_playlist.save
       redirect_to request.referer, notice: "プレイリストに追加しました"
